d55a16d9708e2dccebb61345b9fd08a2d41abc7b
Jinghan <phantom.there@gmail.com>
2016-09-12 22:23:22 +0800

    differentiate viewed ones

--- Wishes/www/js/controllers/ActivityViewController.js
+++ Wishes/www/js/controllers/ActivityViewController.js
@@ -10,30 +10,51 @@ module.controller('ActivityCtrl', function($scope) {
         "WISH_PICKED": "ion-leaf",
     }
 
+    $scope.viewedClass = {
+        true: "viewed",
+        false: ""
+    }
+
     //fake data
     $scope.activities = [{
         type: "WISH_PICKED",
         description: "Your wish has been picked up!",
-        time: "2016-09-09 12:32:00"
+        time: "2016-09-09 12:32:00",
+        viewed: false
     },{
         type: "LEVEL_UP",
         description: "You have achieved level 3!",
-        time: "2016-09-09 12:32:00"
+        time: "2016-09-09 12:32:00",
+        viewed: false
     },{
         type: "WISH_ACCOMPLISHED",
         description: "Congrats! Wish fulfilled!",
-        time: "2016-09-09 12:32:00"
+        time: "2016-09-09 12:32:00",
+        viewed: true
     },{
         type: "WISH_PICKED",
         description: "Your wish has been picked up!",
-        time: "2016-09-09 12:32:00"
+        time: "2016-09-09 12:32:00",
+        viewed: true
     },{
         type: "LEVEL_UP",
         description: "You have achieved level 3!",
-        time: "2016-09-09 12:32:00"
+        time: "2016-09-09 12:32:00",
+        viewed: true
     },{
         type: "WISH_ACCOMPLISHED",
         description: "Congrats! Wish fulfilled!",
-        time: "2016-09-09 12:32:00"
+        time: "2016-09-09 12:32:00",
+        viewed: true
+    },{
+        type: "LEVEL_UP",
+        description: "Congrats! Wish fulfilled!",
+        time: "2016-09-09 12:32:00",
+        viewed: true
+    },{
+        type: "WISH_ACCOMPLISHED",
+        description: "Congrats! Wish fulfilled!",
+        time: "2016-09-09 12:32:00",
+        viewed: true
     }]
 })
