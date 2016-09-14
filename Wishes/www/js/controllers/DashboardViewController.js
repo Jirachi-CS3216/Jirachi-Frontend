a99065864d8e586695a00106e09f27bb53a26874
Jinghan <phantom.there@gmail.com>
2016-09-14 18:52:30 +0800

    open modal instead of url redirect for post new page

--- Wishes/www/js/controllers/DashboardViewController.js
+++ Wishes/www/js/controllers/DashboardViewController.js
@@ -1,5 +1,34 @@
-module.controller('DashCtrl', function($scope) {
+module.controller('DashCtrl', function($scope, $ionicModal) {
 	
+	$ionicModal.fromTemplateUrl('../../templates/dashboard-modal-post.html', {
+	    scope: $scope,
+	    animation: 'slide-in-up'
+	}).then(function(modal) {
+	    $scope.postModal = modal;
+	});
+
+	$scope.openModal = function() {
+	    $scope.postModal.show();
+	};
+		$scope.closeModal = function() {
+		$scope.postModal.hide();
+	};
+
+	// Cleanup the modal when we're done with it!
+	$scope.$on('$destroy', function() {
+		$scope.postModal.remove();
+	});
+
+	// Execute action on hide modal
+	$scope.$on('modal.hidden', function() {
+		// Execute action
+	});
+
+	// Execute action on remove modal
+	$scope.$on('modal.removed', function() {
+		// Execute action
+	});
+
 	$scope.buttonAnimations = {
 		"SELF": "",
 		"OTHER": ""
