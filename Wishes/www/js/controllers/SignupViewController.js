9983300ede2e6a89a4238d0b9d7d9b94e0da9f2e
Jinghan <phantom.there@gmail.com>
2016-09-17 00:37:56 +0800

    validation for signup page

--- Wishes/www/js/controllers/SignupViewController.js
+++ Wishes/www/js/controllers/SignupViewController.js
@@ -6,4 +6,29 @@ module.controller('SignupCtrl', function($scope, $state, $ionicHistory, $ionicSc
 		});
 		$state.go('login');
 	}
+
+	$scope.submitForm = function(isValid, data) {
+		if (isValid) {
+			console.log("Valid form ready to go")
+		}
+	}
+})
+
+.directive("compareTo", function() {
+	return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+             
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+ 
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
 })
