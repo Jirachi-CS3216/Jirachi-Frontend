fdb1a99d2845ae8a7396a8dc6a4e1ab323d1c571
Jinghan <phantom.there@gmail.com>
2016-09-17 03:47:58 +0800

    local cache for updated username and password

--- Wishes/www/js/controllers/SettingsViewController.js
+++ Wishes/www/js/controllers/SettingsViewController.js
@@ -4,6 +4,8 @@ module.controller('SettingsCtrl', function($scope, auth, session) {
 
 	$scope.$on("$ionicView.beforeEnter", function(event, data){
    		$scope.displayName = session.currentUser().display_name
+   		$scope.email = session.currentUser().email
+   		$scope.phone = session.currentUser().phone
 	});
 
     $scope.logout = function() {
@@ -12,6 +14,16 @@ module.controller('SettingsCtrl', function($scope, auth, session) {
 
     $scope.userDisplayNameInputDidChange = function(newValue) {
     	session.currentUser().display_name = newValue;
+    	session.saveContext();
+    }
+
+    $scope.userEmailInputDidChange = function(newValue) {
+    	session.currentUser().email = newValue;
+    	session.saveContext();
+    }
+
+    $scope.userPhoneInputDidChange = function(newValue) {
+    	session.currentUser().phone = newValue;
     	session.saveContext();
     }
 })
