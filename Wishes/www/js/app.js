3a5ac40facddf6f5c72b5224631e78412f87cd5c
Jinghan <phantom.there@gmail.com>
2016-09-19 06:27:44 +0800

    hide status bar on iOS devices

--- Wishes/www/js/app.js
+++ Wishes/www/js/app.js
@@ -42,7 +42,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
+      StatusBar.hide()
     }
   });
 })
