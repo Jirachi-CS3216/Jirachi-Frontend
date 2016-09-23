cfccd9d80ad3d983d3605970d3fbfc4995cdef0d
Jinghan <phantom.there@gmail.com>
2016-09-23 19:57:26 +0800

    handler link for offline messages

--- Wishes/www/js/services/offlineWishPosting.js
+++ Wishes/www/js/services/offlineWishPosting.js
@@ -7,7 +7,7 @@ service('offlineWishPosting', function offlineWishPosting($window, session, apis
 	this.postFromDisk = function() {
 		try {
 			var wish = JSON.parse($window.localStorage[LOCAL_STORAGE_ID]);
-			this.postWish(wish);
+			this.postWish(wish, undefined, true);
 		} catch (e) {
 		}
 	}
@@ -24,23 +24,26 @@ service('offlineWishPosting', function offlineWishPosting($window, session, apis
 		saveToDisk(null);
 	}
 
-	this.postWish = function(wish, handler) {
+	this.postWish = function(wish, handler, fromDisk) {
 		if (navigator.onLine) {
 			apis.wishes.post(session.currentUserID(), {}, wish).success(function(data, status){
 				if (!data.error) {
 					clearFromDisk();
 					$rootScope.$broadcast("notification-should-show", {
-						iconClass: "ion-alert-circled",
-						title: "Wish Posted",
+						iconClass: "ion-checkmark-circled",
+						title: fromDisk ? "Postponed Wish Posted" : "Wish Posted",
 						message: "You wish has been posted to the community."
 					});
+					handler(true)
 				} else if (data.error.points) {
 					$rootScope.$broadcast("notification-should-show", {
 						iconClass: "ion-alert-circled",
-						title: "Not Enough Points",
+						title: fromDisk ? "Postponed Wish: Not Enough Points" : "Not Enough Points",
 						message: "Points not enough :( Try to pick and fulfill others\' wishes to earn points!'"
 					});
+					handler(false)
 				} else {
+					handler(false)
 				}
 			}).error(function(data, status) {
 				handler(false)
@@ -48,10 +51,11 @@ service('offlineWishPosting', function offlineWishPosting($window, session, apis
 		} else {
 			saveToDisk(wish);
 			$rootScope.$broadcast("notification-should-show", {
-				iconClass: "ion-alert-circled",
+				iconClass: "ion-archive",
 				title: "Offline Wish Postponed",
 				message: "Stay calm, you wish will be posted once you go online :) "
 			});
+			handler(true)
 		}
 	}
 });
